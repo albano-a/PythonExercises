d402337fe2307b3b9096a5b8da092c41a8326f41
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2025-01-17 19:34:56 -0300

    Added Aula1.py

---
+++ Python_Curso/aula1.py
@@ -0,0 +1,55 @@
+# Tipos de Dados
+nome = "André Luiz"  # String
+
+idade = 25  # Inteiro
+altura = 1.73  # Float
+
+ativo = True  # Booleano
+
+# print(nome, type(nome))
+
+x = 10
+y = 3
+
+# Operações básicas
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x / y
+potencia = x**y
+
+# print(soma, subtracao, multiplicacao, divisao, potencia)
+
+# Concatenação de Strings
+numero1 = "André"
+numero2 = "Luiz"
+
+# print(numero1 + numero2)
+
+# Conversão de tipo
+# str, int, float, bool
+idade = str(idade)
+# print(type(idade))
+numero3 = "200"
+numero4 = 9.86
+# print(type(numero3))
+numero3 = int(numero3)
+# print(type(numero3))
+
+numero4 = int(numero4)
+print(numero4)
+
+# 3 formas de printar variáveis com texto
+# print("Meu nome é", nome, "e eu tenho", idade, "anos.")
+# print("Meu nome é " + nome + " e eu tenho " + idade + " anos.")
+# print(f"Meu nome é {nome} e eu tenho {idade} anos")
+
+# Listas - são coleções de elementos
+
+numeros = [1, 2, 3, 4]
+print(numeros)
+
+# Dicionários - são coleções de pares chave-valor
+
+pessoa = {"nome":"André", "idade":25, "altura":1.75}
+print(pessoa)
